aee44207e8ba7d1d8c375a2f518daa34dab9768e
Jacob King <jacob.l.king@outlook.com>
2025-03-03 10:29:19 -0600

    test

--- include/generated/officers.ts
+++ include/generated/officers.ts
@@ -1,213 +1,243 @@
 // THIS FILE WAS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED BY HAND!
 export interface Officer {
-    title: string;
-    name: string;
-    linkedIn: string;
-    year: string;
-    semester: string;
-    imageUrl: string;
+	title: string;
+	name: string;
+	linkedIn: string;
+	year: string;
+	semester: string;
+	imageUrl: string;
 }
-export const officers: Officer[] = [{
-        title: "President",
-        name: "Tony Flores",
-        linkedIn: "https://www.linkedin.com/in/tonyfloresf/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "VP of Operations",
-        name: "Apraajita Sharma",
-        linkedIn: "",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "VP of Administration",
-        name: "Saloni Sharma",
-        linkedIn: "https://www.linkedin.com/in/saloni-sharma-scholar/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "VP of Internal Relations",
-        name: "Yousif Saieb",
-        linkedIn: "https://www.linkedin.com/in/saieby/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "VP of External Relations",
-        name: "Jonathan Cummins",
-        linkedIn: "https://www.linkedin.com/in/jonathan-cummins-58b326278/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Artists",
-        name: "Widyan Hussien",
-        linkedIn: "https://www.linkedin.com/in/widyan-hussien/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Historian",
-        name: "Adaeze Owunna",
-        linkedIn: "",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Marketing Director",
-        name: "Tarashree Dorairaj",
-        linkedIn: "https://www.linkedin.com/in/tarashreedorairaj/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Marketing Officer",
-        name: "Lulu Sartaj Mohammad",
-        linkedIn: "https://www.linkedin.com/in/lulu-sartaj-mohammad/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Intern Success Director",
-        name: "Reyna Obregon",
-        linkedIn: "https://www.linkedin.com/in/reyna-obreg%C3%B3n-8779322a8/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Events Logistics Director",
-        name: "Joseph Giap",
-        linkedIn: "https://www.linkedin.com/in/joseph-giap/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Teams Director",
-        name: "Emma Nguyen",
-        linkedIn: "https://www.linkedin.com/in/emma-nguyen05/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Activities Director",
-        name: "Angela Maria",
-        linkedIn: "https://www.linkedin.com/in/angelam-abrea",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Team Management Officer",
-        name: "Evan Fraustro",
-        linkedIn: "https://www.linkedin.com/in/evan-fraustro/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Team Management Officer",
-        name: "Phi Tran",
-        linkedIn: "https://www.linkedin.com/in/pntran25/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Team Management Officer",
-        name: "Miguel Rodriguez",
-        linkedIn: "https://www.linkedin.com/in/miguel-rodriguez302002/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Member Relations Director",
-        name: "Naim Momin",
-        linkedIn: "https://www.linkedin.com/in/naim-momin-071237191/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Corporate Relations Director",
-        name: "Gelila Habte",
-        linkedIn: "https://www.linkedin.com/in/gelila-habte/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Corporate Relations Officer",
-        name: "Zahi Rafidi",
-        linkedIn: "https://www.linkedin.com/in/zahirafidi/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Software Development Director",
-        name: "Jacob King",
-        linkedIn: "https://www.linkedin.com/in/jacob-l-king/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Software Development Officer",
-        name: "Henry Nguyen",
-        linkedIn: "https://www.linkedin.com/in/duc-nguyen-b27425287/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Software Development Officer",
-        name: "Nathan Samuell",
-        linkedIn: "https://www.linkedin.com/in/nathan-samuell/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Professional Development Director",
-        name: "Heera Shetty",
-        linkedIn: "https://www.linkedin.com/in/heera-shetty-0036b328b/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Workshops Director",
-        name: "Mark Pelico",
-        linkedIn: "https://www.linkedin.com/in/mark-pelico-226328327/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Competitions Director",
-        name: "Mohamed Abdelrahman",
-        linkedIn: "https://www.linkedin.com/in/msabdelrahman02/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Collaborations Director",
-        name: "Giang Dao",
-        linkedIn: "https://www.linkedin.com/in/giang-dao-4b4736276/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "DevOps Workshops Officer",
-        name: "Abdullah Shittu",
-        linkedIn: "https://www.linkedin.com/in/abdullah-shittu/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Full Stack Workshops Officer",
-        name: "Alexander Sangurima",
-        linkedIn: "https://www.linkedin.com/in/asangurima/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }, {
-        title: "Data Science Workshops Officer",
-        name: "Dominic McDonald",
-        linkedIn: "https://www.linkedin.com/in/dominic-mcdonald-uh/",
-        year: "2025",
-        semester: "Spring",
-        imageUrl: ""
-    }];
+export const officers: Officer[] = [
+	{
+		title: "President",
+		name: "Tony Flores",
+		linkedIn: "https://www.linkedin.com/in/tonyfloresf/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "VP of Operations",
+		name: "Apraajita Sharma",
+		linkedIn: "",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "VP of Administration",
+		name: "Saloni Sharma",
+		linkedIn: "https://www.linkedin.com/in/saloni-sharma-scholar/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "VP of Internal Relations",
+		name: "Yousif Saieb",
+		linkedIn: "https://www.linkedin.com/in/saieby/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "VP of External Relations",
+		name: "Jonathan Cummins",
+		linkedIn: "https://www.linkedin.com/in/jonathan-cummins-58b326278/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Artists",
+		name: "Widyan Hussien",
+		linkedIn: "https://www.linkedin.com/in/widyan-hussien/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Historian",
+		name: "Adaeze Owunna",
+		linkedIn: "",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Marketing Director",
+		name: "Tarashree Dorairaj",
+		linkedIn: "https://www.linkedin.com/in/tarashreedorairaj/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Marketing Officer",
+		name: "Lulu Sartaj Mohammad",
+		linkedIn: "https://www.linkedin.com/in/lulu-sartaj-mohammad/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Intern Success Director",
+		name: "Reyna Obregon",
+		linkedIn: "https://www.linkedin.com/in/reyna-obreg%C3%B3n-8779322a8/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Events Logistics Director",
+		name: "Joseph Giap",
+		linkedIn: "https://www.linkedin.com/in/joseph-giap/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Teams Director",
+		name: "Emma Nguyen",
+		linkedIn: "https://www.linkedin.com/in/emma-nguyen05/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Activities Director",
+		name: "Angela Maria",
+		linkedIn: "https://www.linkedin.com/in/angelam-abrea",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Team Management Officer",
+		name: "Evan Fraustro",
+		linkedIn: "https://www.linkedin.com/in/evan-fraustro/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Team Management Officer",
+		name: "Phi Tran",
+		linkedIn: "https://www.linkedin.com/in/pntran25/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Team Management Officer",
+		name: "Miguel Rodriguez",
+		linkedIn: "https://www.linkedin.com/in/miguel-rodriguez302002/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Member Relations Director",
+		name: "Naim Momin",
+		linkedIn: "https://www.linkedin.com/in/naim-momin-071237191/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Corporate Relations Director",
+		name: "Gelila Habte",
+		linkedIn: "https://www.linkedin.com/in/gelila-habte/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Corporate Relations Officer",
+		name: "Zahi Rafidi",
+		linkedIn: "https://www.linkedin.com/in/zahirafidi/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Software Development Director",
+		name: "Jacob King",
+		linkedIn: "https://www.linkedin.com/in/jacob-l-king/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Software Development Officer",
+		name: "Henry Nguyen",
+		linkedIn: "https://www.linkedin.com/in/duc-nguyen-b27425287/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Software Development Officer",
+		name: "Nathan Samuell",
+		linkedIn: "https://www.linkedin.com/in/nathan-samuell/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Professional Development Director",
+		name: "Heera Shetty",
+		linkedIn: "https://www.linkedin.com/in/heera-shetty-0036b328b/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Workshops Director",
+		name: "Mark Pelico",
+		linkedIn: "https://www.linkedin.com/in/mark-pelico-226328327/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Competitions Director",
+		name: "Mohamed Abdelrahman",
+		linkedIn: "https://www.linkedin.com/in/msabdelrahman02/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Collaborations Director",
+		name: "Giang Dao",
+		linkedIn: "https://www.linkedin.com/in/giang-dao-4b4736276/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "DevOps Workshops Officer",
+		name: "Abdullah Shittu",
+		linkedIn: "https://www.linkedin.com/in/abdullah-shittu/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Full Stack Workshops Officer",
+		name: "Alexander Sangurima",
+		linkedIn: "https://www.linkedin.com/in/asangurima/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+	{
+		title: "Data Science Workshops Officer",
+		name: "Dominic McDonald",
+		linkedIn: "https://www.linkedin.com/in/dominic-mcdonald-uh/",
+		year: "2025",
+		semester: "Spring",
+		imageUrl: "",
+	},
+];
